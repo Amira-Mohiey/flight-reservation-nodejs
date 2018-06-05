41c72f18aa080bc77828b81be1c5ae00317c20ff
amira-mohiey <amira.mohiey@cartologic.com>
2018-06-05 20:01:17 +0200

    allow put method

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ var seatRouter = require(__dirname+'/app/controllers/seat.js')
 var userRouter = require(__dirname+'/app/controllers/user.js')
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET", "PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 app.use('/seats', seatRouter)
